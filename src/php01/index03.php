7415ae6aae12143ba9aff4c0e6018b1a81c93fbf
area24 <areastation24@gmail.com>
2024-03-12 10:43:34 +0900

    変更

---
+++ src/php01/index03.php
@@ -0,0 +1,21 @@
+<?php
+    $a = 15;
+    $b = 3;
+    $c = 10;
+
+    $answer1 = $a + $b;  // 18 = 15 + 3
+    $answer2 = $a - $b;  // 12 = 15 - 3
+    $answer3 = $a * $b;  // 45 = 15 * 3
+    $answer4 = $a / $b;  //  5 = 15 / 3    
+    $answer5 = $a % $c;  //  5 = 15 % 3
+
+    echo $answer1;  // 18
+    echo "<br />";
+    echo $answer2;  // 12
+    echo "<br />";
+    echo $answer3;  // 45
+    echo "<br />";
+    echo $answer4;  //  5
+    echo "<br />";
+    echo $answer5;  //  5
+?>
