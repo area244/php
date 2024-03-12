107f451a6465116e0dbd8cce5dff691b62eb5676
area24 <areastation24@gmail.com>
2024-03-12 21:56:41 +0900

    変更

---
+++ src/php03/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta name="robots" content="noindex,nofollow" />
+		<link rel="stylesheet" href="css/sanitize.css">
+		<link rel="stylesheet" href="css/common.css">
+		<link rel="stylesheet" href="css/index.css">
+		<title>1-PHP_世界の時計</title>
+	</head>
+
+	<body>
+  		<header class="header">
+    		<div class="header__inner">
+      			<a class="header__logo" href="/php03/index.php">
+        			World Clock
+      			</a>
+    		</div>
+  		</header>
+	</body>
+</html>
