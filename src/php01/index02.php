137ff4babee0a57ecf7b69645046d0206da88e1a
area24 <areastation24@gmail.com>
2024-04-22 13:47:38 +0900

    リモートリポジトリの変更

---
+++ src/php01/index02.php
@@ -0,0 +1,53 @@
+<?php
+	// 1-1
+	$item = "JavaScript";
+	$item = "PHP";
+	echo $item;  // PHP
+	print '<br>';
+	
+	$item = "JavaScript";
+	echo $item;
+	print '<br>';
+	print '<br>';
+
+    // 2-1
+	const FELANGUAGE = "Vue.js";
+	echo FELANGUAGE;
+	print '<br>';
+	print '<br>';
+
+    // 3-1
+	$item = "PHP";  // PHP
+	echo $item;
+	print '<br>';
+	print '<br>';
+
+    // 3-2
+	$item = 123; // 123
+	echo $item;
+	print '<br>';
+	print '<br>';
+
+    // 4-1
+	$name1 = "Sato" . "Taro";
+	$name2 =  "Tanaka";
+	$last_name = "Yamada";
+	$first_name = "Saburo";
+
+	echo $name1;           // SatoTaro
+	echo "<br />";
+	echo $name2 . "Jiro";  // TanakaJiro
+	echo "<br />";
+	echo $last_name . $first_name;  // YamadaSaburo
+	print '<br>';
+	print '<br>';
+
+    // Q4-1
+	$name =  "Tanaka";
+	$last_name = "Yamada";
+	$first_name = "Saburo";
+
+	echo $name . "Jiro";	// TanakaJiro
+	echo "<br />";
+	echo $last_name . $first_name; // YamadaSaburo
+	print '<br>';
